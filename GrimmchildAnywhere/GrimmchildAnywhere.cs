13be32f7df2da64cb385c3c63df3a6ce0e0f2845
Exempt-Medic <smdemieri@email.wm.edu>
2022-12-01 11:10:43 -0500

    Add project files.

---
+++ GrimmchildAnywhere/GrimmchildAnywhere.cs
@@ -0,0 +1,48 @@
+using Modding;
+using System;
+using SFCore.Utils;
+
+namespace GrimmchildAnywhere
+{
+    public class GrimmchildAnywhereMod : Mod
+    {
+        private static GrimmchildAnywhereMod? _instance;
+
+        internal static GrimmchildAnywhereMod Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    throw new InvalidOperationException($"An instance of {nameof(GrimmchildAnywhereMod)} was never constructed");
+                }
+                return _instance;
+            }
+        }
+
+        public override string GetVersion() => GetType().Assembly.GetName().Version.ToString();
+
+        public GrimmchildAnywhereMod() : base("GrimmchildAnywhere")
+        {
+            _instance = this;
+        }
+
+        public override void Initialize()
+        {
+            Log("Initializing");
+
+            On.PlayMakerFSM.OnEnable += OnFsmEnable;
+
+            Log("Initialized");
+        }
+        private void OnFsmEnable(On.PlayMakerFSM.orig_OnEnable orig, PlayMakerFSM self)
+        {
+            orig(self);
+
+            if (self.gameObject.name == "Charm Effects" && self.FsmName == "Spawn Grimmchild")
+            {
+                self.ChangeFsmTransition("Dream?", "FINISHED", "Charms Allowed?");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+﻿# GrimmchildAnywhere
+
+A Hollow Knight mod that allows Grimmchild to spawn during TMG and NKG fights. Does not work with Dirtmouth TMG.
+
+Requires:
+* SFCore
